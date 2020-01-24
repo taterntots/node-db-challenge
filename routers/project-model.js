0e29d5ad1f430e2980190ee481930ce7931c0ae2
taterntots <matthew.a.weidner@gmail.com>
2020-01-24 12:48:46 -0500

    can now get a list of and add new projects

--- routers/project-model.js
+++ routers/project-model.js
@@ -1,12 +1,31 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  getProjects
+  getProjects,
+  addProject
 }
 
 // *****************************************
-// gets a list of projects
+// gets a list of all projects
 // *****************************************
 function getProjects() {
-  
+  return db('projects');
+}
+// *****************************************
+// gets a specific project by id
+// *****************************************
+function getProjectById(projectId) {
+  return db('projects')
+    .where({ id: projectId})
+    .first();
+}
+// *****************************************
+// add a new project to the list
+// *****************************************
+function addProject(newProject) {
+  return db('projects')
+    .insert(newProject)
+    .then(([id]) => {
+      return getProjectById(id);
+    })
 }
--- routers/project-router.js
+++ routers/project-router.js
@@ -3,10 +3,35 @@ const Pdb = require('./project-model');
 const router = express.Router();
 
 // *****************************************
-// gets a list of projects
+// gets a list of all projects
 // *****************************************
 router.get('/', (req, res) => {
-  
+  Pdb.getProjects()
+    .then(projects => {
+      projects.map(project => {
+        if (project.completed === 0) {
+          project.completed = false;
+        } else {
+          project.completed = true;
+        }
+      })
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'There was an error retrieving the list of projects' });
+    })
+})
+// *****************************************
+// add a new project to the list
+// *****************************************
+router.post('/', (req, res) => {
+  Pdb.addProject(req.body)
+    .then(newProject => {
+      res.status(201).json(newProject);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'There was an error adding the project' });
+    })
 })
 
 module.exports = router;
